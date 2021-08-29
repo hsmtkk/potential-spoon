a90257c75bf943679d79463fbcdc4f3e0e676dd1
kouki <kouki@localhost>
2021-08-29 11:35:35 +0900

    fine

--- src/json_zipcode_handler.rs
+++ src/json_zipcode_handler.rs
@@ -1,11 +1,11 @@
 use actix_web::{HttpRequest, HttpResponse};
+use actix_web::web::Data;
 use crate::zipcode_api::Searcher;
 use log::info;
 
-pub async fn handle(req: HttpRequest) -> HttpResponse {
+pub async fn handle(req: HttpRequest, searcher:Data<Searcher>) -> HttpResponse {
     let zipcode = req.match_info().get("zipcode").unwrap();
     info!("json_zipcode_handler {}", zipcode);
-    let searcher = Searcher::new();
     let json = searcher.search(zipcode).unwrap();
     HttpResponse::Ok()
     .content_type("application/json")
